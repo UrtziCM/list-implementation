a8791139a0f24bb82688903f3ed60696e43a6acc
Yxini <urcrmedev@gmail.com>
2025-02-21 17:23:43 +0100

    fix: fixed memory leaks and segfaults

--- List.cpp
+++ List.cpp
@@ -7,54 +7,56 @@ namespace Euneiz
 	// Node stuff
 	Node::Node(int data) {
 		this->data = data;
-		this->next = nullptr;
+		next = nullptr;
 	}
-	
-	Node::~Node() {
-		//delete this->next;
-	}
-	
+
+	Node::~Node() {}
+
 	// List stuff
-	
-    List::List() {
+
+	List::List() {
 		this->size = 0;
 		this->head = nullptr;
 		this->tail = nullptr;
 	}
+
 	List::~List() {
 		Clear();
-		//delete this->head;
-		//delete this->tail;
 	}
+
 	void List::PushBack(int data) {
-		size++;
-		if (head) {
-			Node* tempNodePointer = tail;
-			tempNodePointer->next=CreateNode(data);
-			tail = tempNodePointer->next;
-		} else {
-			head = CreateNode(data);
+		Node* newNode = CreateNode(data);
+		if (size > 0) {
+			Node* tempTailNode = tail;
+
+			tempTailNode->next = newNode;
+			tail = newNode;
+		}
+		else {
+			head = newNode;
 			tail = head;
 		}
-	}
-	
-	void List::PushFront(int data) {
 		size++;
-		if (head) {			
+	}
+
+	void List::PushFront(int data) {
+		if (head) {
 			Node* newHead = CreateNode(data);
 			newHead->next = this->head;
 			this->head = newHead;
-		} else {
+		}
+		else {
 			head = CreateNode(data);
 			tail = head;
 		}
+		size++;
 	}
-	
-	void List::PopFront(){ 
-		size--;
+
+	void List::PopFront() {
 		Node* nodeToDelete = this->head;
 		this->head = nodeToDelete->next;
 		delete nodeToDelete;
+		size--;
 	}
 	bool List::Empty() {
 		return size == 0;
@@ -63,106 +65,125 @@ namespace Euneiz
 		return size;
 	}
 	void List::Clear() {
-		if (size > 0) {
-			Node* current_node = (*this).head;
+		if (size == 1) { // Si no, al hacer erase de el primero siendo 1 solo, peta
+			PopFront();
+		}else if (size > 1) {
+			Node* current_node = this->head;
 			Node* next_node;
-			while(current_node != nullptr)	{
+			while (current_node != nullptr) {
 				next_node = current_node->next;
 				delete current_node;
 				current_node = next_node;
 			}
+			next_node = nullptr;
 		}
+		head = nullptr;
+		tail = nullptr;
 		size = 0;
 	}
-	
-	void List::Insert(int data, int index){
+
+	void List::Insert(int data, int index) {
 		if (index >= size) {
 			PushBack(data);
-		} else if (index <= 0) {
-			
-			PushFront(data);
-		} else { // tomamos previous y next del índice y actualizamos pointers
-			Node* previousNode = nullptr;
-			Node* nextNode = nullptr;
-			int currentIndex = 0;
-			for (Node* currentNode = head; currentIndex < index; currentIndex++) {
-				if (currentIndex == 0)
-					previousNode = currentNode;
-					continue;
-				nextNode = currentNode;
-				if (currentIndex == index) {
-					break;
-				}			
-				previousNode = currentNode;
-			}
-			Node* newNode = CreateNode(data);
-			previousNode->next = newNode;
-			newNode->next = nextNode;
 		}
-		
-		size++;	
-	} // TODO: Implement
-	
-	void List::Erase(int index){
-		if (this->Size() == 0) {
+		else if (index <= 0) {
+			PushFront(data);
+		}
+		else { // tomamos previous y next del índice y actualizamos pointers
+			Node* previousNode = head;
+			Node* currentNode = head->next;
+			Node* nextNode = nullptr;
+
+			// Empezamos en el indice 1 porque current es el 1 y previous es el 0
+
+			for (int currentIndex = 1; currentIndex < size; currentIndex++) {
+				if (currentIndex == index) {
+					nextNode = currentNode;
+					break;
+				}
+				else {
+					previousNode = currentNode;
+					currentNode = currentNode->next;
+				}
+			}
+			currentNode = CreateNode(data);
+			previousNode->next = currentNode;
+			currentNode->next = nextNode;
+			size++;
+		}
+
+	}
+
+	void List::Erase(int index) { // REDO AFTER VOLLEY
+		if (!head || size <= 0) {
 			return;
 		}
-		size--;
-		
-		if (size == 1 && index == 0) { // Y quieren borrar el primero: Clear
-			this->Clear();
-		} else if(size == 2) { // Y quieren borrar 1, head y tail pasan a ser el que no borra
-			switch (index) {
-				case 0:
+		if (index > size - 1) { // Clamp values between 0 and size
+			index = size - 1;
+		}
+		else if (index < 0) {
+			index = 0;
+		}
+
+		switch (size) {
+			case 1:
+				Clear();
+				break;
+			case 2:
+				if (index == 0) {
+					PopFront();
+				} else {
+					Node* nodeToDelete = this->tail;
+					this->tail = head;
+					delete nodeToDelete;
+				}
+				size--;
+				break;
+			default: // Size is at least 3 so we go for the wanted index and the previous one
+				if (index == 0) {
+					Node* tempNewHead = head->next;
+					size--;
 					delete head;
-					head = tail;
-					break;
-				case 1:
-					delete tail;
-					tail = head;
-					break;
-			}
-		} else if (index == size - 1) { // Si se quiere borrar el último...
-			Node* previousNode = nullptr;
-			for (Node* currentNode = head; currentNode != tail;) {		
-				previousNode = currentNode;
-			}
-			delete tail;
-			tail = previousNode;
-			
-		} else { // Iteramos hasta tener los 3 (previous, current, next) y previous apunta a next
-			Node* previousNode = nullptr;
-			Node* nodeToDelete = nullptr;
-			Node* nextNode = nullptr;
-			int currentIndex = 0;
-			for (Node* currentNode = head; currentIndex <= index; currentIndex++) {
-				if (currentIndex == 0)
-					previousNode = currentNode;
-					continue;
-				nodeToDelete = currentNode;
-				nextNode = currentNode->next;
-				
-				previousNode = currentNode;
-			}
-			previousNode->next = nextNode;
-			delete nodeToDelete; 
-			
-		}		
+					head = tempNewHead;
+				} else {
+					Node* previousNode = head;
+					Node* currentNode = head->next;
+					Node* nextNode = nullptr;
+
+					// Empezamos en el indice 1 porque current es el 1 y previous es el 0
+
+					for (int currentIndex = 1; currentIndex < size; currentIndex++) {
+						if (currentIndex == index) {
+							nextNode = currentNode->next;
+							break;
+						}
+						else {
+							previousNode = currentNode;
+							currentNode = currentNode->next;
+						}
+					}
+					size--;
+					delete currentNode;
+					previousNode->next = nextNode;
+					if (!nextNode) { // If its null pointer
+						tail = previousNode;
+					}
+				}
+				break;
+		}
 	}
-	
+
 	Node* List::CreateNode(int data) {
-		Node* node = new Node(data);
-		return node;
+		return new Node(data);
 	}
-	
-    std::ostream& operator <<(std::ostream& os, const List& list) {
+
+	std::ostream& operator <<(std::ostream& os, const List& list) {
 		Node* current_node = list.head;
-		while(current_node) {
+		while (current_node) {
 			os << std::setw(4) << current_node->data;
 			current_node = current_node->next;
 		}
 		os << std::endl;
 		return os;
 	}
-	
 }
